c64a3489179bb70b06b139b1ff50d6564f8747bb
DougMobitype <douglas.wenzler@mobitype.com.br>
2022-09-13 21:39:33 -0300

    Second class

---
+++ src/components/GameCard/index.tsx
@@ -0,0 +1,33 @@
+import { LinearGradient } from 'expo-linear-gradient';
+import { TouchableOpacity, TouchableOpacityProps, ImageBackground, ImageSourcePropType, Text } from 'react-native';
+import { THEME } from '../../theme';
+
+import { styles } from './styles';
+
+export interface GameCardProps {
+    id: string;
+    name: string;
+    ads: string;
+    cover: ImageSourcePropType;
+}
+
+interface Props extends TouchableOpacityProps {
+    data: GameCardProps
+}
+
+export function GameCard({ data, ...rest }: Props) {
+    return (
+        <TouchableOpacity style={styles.container}>
+            <ImageBackground source={data.cover} style={styles.cover}>
+                <LinearGradient colors={THEME.COLORS.FOOTER} style={styles.footer}>
+                    <Text style={styles.name}>
+                        {data.name}
+                    </Text>
+                    <Text style={styles.ads}>
+                        {data.ads} anúncios
+                    </Text>
+                </LinearGradient>
+            </ImageBackground>
+        </TouchableOpacity>
+    );
+}
